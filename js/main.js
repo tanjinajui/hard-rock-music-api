581fe3d926001363dd85c1d1c62a2b6879079c01
tanjinajui <tanjinaakter994@gmail.com>
2022-02-05 10:28:20 +0600

    index-js-file

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// let lyricsMusic = {
+//     fetchLyrics:function(){
+//         fetch(
+//             "https://api.lyrics.ovh/suggest/baby"
+//         )
+//         .then((response)=>response.json())
+//         .then(data => console.log(data))
+        
+//     }
+// }
+
+const searchSongs = () => {
+    const searchText = document.getElementById("search-song").value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    fetch(url)
+        .then((response)=>response.json())
+        .then(data => displaySongs(data.data))
+        .catch(error => displayError('Something went wrong! Please try again later!'));
+    }
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML =`
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+            <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}','${song.title}) class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+        songContainer.appendChild(songDiv);
+        }) 
+}
+const getLyric = async (artist,title) =>{
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    try{
+        const res = await fetch(url);
+        const data= await res.json();
+        displayLyrics(data.lyrics);
+    }
+    catch{
+        displayError('Sorry! I failed to load lyrics, Please try again later!!!')
+    }
+}
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('single-lyrics');
+    lyricsDiv.innerText = lyrics;
+}
+const displayError = error => {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.innerText = error;
+}
